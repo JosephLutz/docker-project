34fb3d6146332d4d5416c5fdd046a83d0d8e1ff5
Joseph A. Lutz <joseph.lutz@novatechweb.com>
2015-03-13 14:09:00 -0500

    moving http-base back into websvn
    Signed-off-by: Joseph A. Lutz <joseph.lutz@novatechweb.com>

--- http-base/install.sh
+++
@@ -1,8 +0,0 @@
-set -e
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install --yes apache2-utils
-DEBIAN_FRONTEND=noninteractive apt-get install --yes libapache2-svn
-DEBIAN_FRONTEND=noninteractive apt-get install --yes enscript
-DEBIAN_FRONTEND=noninteractive apt-get install --yes websvn
-apt-get clean
-rm -rf /var/lib/apt/lists/*
