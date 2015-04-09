7d71b7cedecaf6f89125599e453a4c284e4efd73
Joseph A. Lutz <joseph.lutz@novatechweb.com>
2015-04-09 16:59:15 -0500

    Config options for new django projects
    Signed-off-by: Joseph A. Lutz <joseph.lutz@novatechweb.com>

--- config.sh
+++ config.sh
@@ -7,15 +7,15 @@ SVN_REPOS=("ddio" "novatech" "NCD_Release")
 GIT_IP=172.16.0.102
 HOST_GIT_BACKUP_DIR=$(pwd)/BACKUP/GIT/
 
+# variables for MediaWIKI
+MEDIAWIKI_IP=172.16.0.104
+HOST_MEDIAWIKI_BACKUP_DIR=$(pwd)/BACKUP/WIKI/
 
-#HTPASSMAN_IP=172.16.0.103
-#HOST_HTPASSMAN_SSL_DIR=$(pwd)/data/htpassman_ssl
-#HOST_HTPASSMAN_PASSWD_DIR=$(pwd)/data/htpassman_password
-#
+# variables for OpenLDAP
+OPENLDAP_IP=172.16.0.105
+HOST_OPENLDAP_BACKUP_DIR=$(pwd)/BACKUP/LDAP/
+
+
 #DJANGO_IP=172.16.0.104
 #HOST_DJANGO_DIR=$(pwd)/code
 #HOST_WEBSVN_PASSWD_DIR=$(pwd)/data/websvn_password
-#
-#TESTSTATION_IP=172.16.0.105
-#HOST_TESTSTATION_MYSQL_DATA_DIR=$(pwd)/teststation_mysql/data
-#HOST_TESTSTATION_DJANGO_CODE_DIR=$(pwd)/httpd_django/Build_System_Code
