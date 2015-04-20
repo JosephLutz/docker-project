985597d3ded948e3056287d7bf62f5ec95ceb1d7
Joseph A. Lutz <joseph.lutz@novatechweb.com>
2015-04-20 16:55:42 -0500

    Changes trying to get LDAP to configure
    Signed-off-by: Joseph A. Lutz <joseph.lutz@novatechweb.com>

--- build.openldap.sh
+++ build.openldap.sh
@@ -12,10 +12,16 @@ set -e
 
 # ************************************************************
 # verify prerequisites:
-sudo docker inspect ${NAME_LDAP_CONTAINER} $> /dev/null && {
+sudo docker inspect ${NAME_LDAP_CONTAINER} &> /dev/null && {
     # The container we are trying to create already exists
     echo "Container already exists : ${NAME_LDAP_CONTAINER}"
     exit 1
+}
+sudo docker inspect ${NAME_LDAP_IMAGE}:latest &> /dev/null && {
+	echo "Image exists: ${NAME_LDAP_IMAGE}:latest"
+	echo "  Can not build or update the image (${NAME_LDAP_IMAGE}:${TAG})"
+	echo "  while ${NAME_LDAP_IMAGE}:latest exists."
+	exit 1
 }
 sudo docker inspect ${NAME_OPENSSL_DV} &> /dev/null || \
     ./build.openssl.sh
@@ -25,7 +31,7 @@ sudo docker inspect debian:8 > /dev/null
 
 # ************************************************************
 # create docker images:
-sudo docker inspect ${NAME_LDAP_IMAGE} &> /dev/null && {
+sudo docker inspect ${NAME_LDAP_IMAGE}:${TAG} &> /dev/null && {
 	# an image already exists with the name and tag we are trying to create.
 	# move it to the latest tag so it will be updated and then renamed
 	sudo docker tag ${NAME_LDAP_IMAGE}:${TAG} ${NAME_LDAP_IMAGE}:latest
@@ -43,18 +49,62 @@ sudo docker rmi ${NAME_LDAP_IMAGE}:latest
 sudo docker inspect ${NAME_LDAP_DV} &> /dev/null || \
     sudo docker run -ti --name "${NAME_LDAP_DV}" \
       -v ${HOST_OPENLDAP_BACKUP_DIR}:/tmp/import_export \
-      ${NAME_LDAP_IMAGE}:${TAG} restore
+      ${NAME_LDAP_IMAGE}:${TAG} init_data_volumes
+
+# setup data volumes
+  #sudo docker run -ti --rm \
+  #  --volumes-from "${NAME_LDAP_DV}" \
+  #  -v ${HOST_OPENLDAP_BACKUP_DIR}:/tmp/import_export \
+  #  ${NAME_LDAP_IMAGE}:${TAG} apply_ldif database.ldif
+sudo docker run -ti --rm \
+  --volumes-from "${NAME_LDAP_DV}" \
+  -v ${HOST_OPENLDAP_BACKUP_DIR}:/tmp/import_export \
+  ${NAME_LDAP_IMAGE}:${TAG} /bin/bash
 
 # ************************************************************
 # Start openldap for running on the linuxserver
-#sudo docker run -d --name "${NAME_LDAP_CONTAINER}" \
-#  --restart=always \
-#  --volumes-from "${NAME_OPENSSL_DV}" \
-#  --volumes-from "${NAME_LDAP_DV}" \
-#  ${NAME_LDAP_IMAGE}:${TAG}
-
-sudo docker run -ti --rm --name "${NAME_LDAP_CONTAINER}" \
-  -P -p ${OPENLDAP}:389 -p ${OPENLDAP_SECURE}:636 \
+# start container running
+sudo docker run -d --name "${NAME_LDAP_CONTAINER}" \
+  --restart=always \
   --volumes-from "${NAME_OPENSSL_DV}" \
   --volumes-from "${NAME_LDAP_DV}" \
-  ${NAME_LDAP_IMAGE}:${TAG} /bin/bash
+  ${NAME_LDAP_IMAGE}:${TAG}
+
+#  -P -p ${OPENLDAP}:389 -p ${OPENLDAP_SECURE}:636 \
+
+
+
+#SLAPADD=/usr/sbin/slapadd
+#    init)
+#        mkdir /tmp/config
+#        cp /config/* /tmp/config/
+#        DC='dc=novatech-llc,dc=com'
+#        echo "Enter in password for olcRootPW :"
+#        password=$(slappasswd)
+#        sed -ie "s|dc=XXXXXXXXXXXX,dc=XXX|"${DC}"|" /tmp/config/config.ldif
+#        #sed -ie "s|dc=XXXXXXXXXXXX,dc=XXX|"${DC}"|" /tmp/config/ldapuser.ldif
+#        sed -ie "s|XXXXXXxxxxxxxxxxxxxxxxxxxxxxxx|"${password}"|" /tmp/config/config.ldif
+#        #sed -ie "s|XXXXXXxxxxxxxxxxxxxxxxxxxxxxxx|"${password}"|" /tmp/config/ldapuser.ldif
+#        # Set OpenLDAP admin password
+#        #ldapadd -Y EXTERNAL -H ldapi:/// -f /tmp/config/config.ldif
+#        #ldapadd -Y EXTERNAL -H ldapi:/// -f /tmp/config/ldapuser.ldif
+#        #ldapadd -x -D cn=Manager,dc=server,dc=world -W -f ldapuser.ldif
+#        nice ${SLAPADD} -F ${CONFIG_PATH} -n 0 -l /tmp/config/config.ldif
+#        #nice ${SLAPADD} -F ${CONFIG_PATH} -n 0 -l /tmp/config/ldapuser.ldif
+#        ;;
+#    backup)
+#        nice ${SLAPCAT} -n 0 > ${BACKUP_PATH}/config.ldif
+#        nice ${SLAPCAT} -n 1 > ${BACKUP_PATH}/domain.ldif
+#        nice ${SLAPCAT} -n 2 > ${BACKUP_PATH}/access.ldif
+#        chmod 640 ${BACKUP_PATH}/*.ldif
+#        ;;
+#    restore)
+#        rm -rf /etc/ldap/* ${DB_PATH}/*
+#        cd /etc/ldap/
+#        tar -xaf /config/ldap.tgz
+#        nice ${SLAPADD} -F ${CONFIG_PATH} -n 0 -l ${BACKUP_PATH}/config.ldif
+#        nice ${SLAPADD} -F ${CONFIG_PATH} -n 1 -l ${BACKUP_PATH}/domain.ldif
+#        nice ${SLAPADD} -F ${CONFIG_PATH} -n 2 -l ${BACKUP_PATH}/access.ldif
+#        chown -R openldap:openldap ${BACKUP_PATH}
+#        chown -R openldap:openldap ${DB_PATH}
+#        ;;
