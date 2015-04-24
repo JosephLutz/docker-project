be2863f28907e3dfa9c7bd0d79adb09229099335
Joseph A. Lutz <joseph.lutz@novatechweb.com>
2015-04-24 17:45:05 -0500

    split phpmyadmin off from mediawiki into it's own group of containers
    Signed-off-by: Joseph A. Lutz <joseph.lutz@novatechweb.com>

--- build.mediawiki.sh
+++ build.mediawiki.sh
@@ -15,11 +15,6 @@ sudo docker inspect ${NAME_WIKI_CONTAINER} &> /dev/null && {
     echo "Container already exists : ${NAME_WIKI_CONTAINER}"
     exit 1
 }
-sudo docker inspect ${NAME_WIKI_PHPMYADMIN_CONTAINER} &> /dev/null && {
-    # The container we are trying to create already exists
-    echo "Container already exists : ${NAME_WIKI_PHPMYADMIN_CONTAINER}"
-    exit 1
-}
 #   pull latest version of base image
 sudo docker inspect ${NAME_OPENSSL_DV} &> /dev/null || \
     ./build.openssl.sh
@@ -27,15 +22,11 @@ sudo docker pull synctree/mediawiki
 sudo docker inspect synctree/mediawiki > /dev/null
 sudo docker pull mysql:latest
 sudo docker inspect mysql:latest > /dev/null
-sudo docker pull corbinu/docker-phpmyadmin:latest
-sudo docker inspect corbinu/docker-phpmyadmin:latest > /dev/null
 
 # ************************************************************
 # remove any old versions of the tags
 sudo docker inspect mysql:${TAG} &> /dev/null && \
     sudo docker rmi mysql:${TAG}
-sudo docker inspect corbinu/docker-phpmyadmin:${TAG} &> /dev/null && \
-    sudo docker rmi corbinu/docker-phpmyadmin:${TAG}
 
 # ************************************************************
 # create docker images:
@@ -53,7 +44,6 @@ sudo docker rmi ${NAME_WIKI_IMAGE}:latest
 
 # create tag for images
 sudo docker tag mysql:latest mysql:${TAG}
-sudo docker tag corbinu/docker-phpmyadmin:latest corbinu/docker-phpmyadmin:${TAG}
 
 # ************************************************************
 # create the data volumes:
@@ -71,7 +61,7 @@ sudo docker inspect ${NAME_WIKI_DV} &> /dev/null || \
 # start mysql
 sudo docker run -d --name "${NAME_WIKI_MYSQL_CONTAINER}" \
   --restart=always \
-  -e MYSQL_ROOT_PASSWORD="${MYSQL_PASSWORD}" \
+  -e MYSQL_ROOT_PASSWORD="${WIKI_MYSQL_PASSWORD}" \
   -e MYSQL_DATABASE=wikidb \
   -e MYSQL_USER="${MEDIAWIKI_USER}" \
   -e MYSQL_PASSWORD="${MEDIAWIKI_PASSWORD}" \
@@ -95,25 +85,3 @@ sudo docker run -d --name "${NAME_WIKI_CONTAINER}" \
 # ************************************************************
 # restore mediawiki settings
 ./mediawiki.sh restore
-
- ### # ************************************************************
- ### # build phpmyadmin with custom changes and tag image
- ### sudo docker build --rm=true --tag="novatechweb/phpmyadmin" ./docker-phpmyadmin
- ### sudo docker inspect novatechweb/phpmyadmin:latest > /dev/null
- ### sudo docker inspect novatechweb/phpmyadmin:${TAG} &> /dev/null && \
- ###     sudo docker rmi novatechweb/phpmyadmin:${TAG}
- ### sudo docker tag novatechweb/phpmyadmin:latest novatechweb/phpmyadmin:${TAG}
-
-# ************************************************************
-# start phpmyadmin and link it to the database
-sudo docker inspect ${NAME_WIKI_MYSQL_CONTAINER} > /dev/null
-sudo docker run -d --name ${NAME_WIKI_PHPMYADMIN_CONTAINER} \
-  --link ${NAME_WIKI_MYSQL_CONTAINER}:mysql \
-  -e MYSQL_USERNAME=root \
-  -e MYSQL_PASSWORD="${MYSQL_PASSWORD}" \
-  -e PMA_SECRET="${PHPMYADMIN_PMA_SECRET}" \
-  -e PMA_USERNAME="${PHPMYADMIN_PMA_USERNAME}" \
-  -e PMA_PASSWORD="${PHPMYADMIN_PMA_PASSWD}" \
-  -p ${PHPMYADMIN_OPEN}:80 \
-  corbinu/docker-phpmyadmin:${TAG}
-#  novatechweb/phpmyadmin:${TAG}
