51660828be76707a23cb5cffa178be7dcf83ac38
Joseph A. Lutz <joseph.lutz@novatechweb.com>
2015-06-03 12:34:53 -0500

    BACKUP_TIMESTAMP is an environment variable and not just a string
    Signed-off-by: Joseph A. Lutz <joseph.lutz@novatechweb.com>

--- docker-gitlab/restore_script.sh
+++ docker-gitlab/restore_script.sh
@@ -5,7 +5,7 @@ set -e
 
 rm -f /home/git/data/backups/*_gitlab_backup.tar
 
-sudo -u git -H bundle exec rake gitlab:backup:restore force=yes BACKUP=BACKUP_TIMESTAMP RAILS_ENV=production
+sudo -u git -H bundle exec rake gitlab:backup:restore force=yes BACKUP=${BACKUP_TIMESTAMP} RAILS_ENV=production
 
 cp --verbose /home/git/data/backups/*_gitlab_backup.tar /tmp/import_export/
 
