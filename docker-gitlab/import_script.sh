a668c251b20a048a91e458d4275251eb8427129d
Joseph A. Lutz <joseph.lutz@novatechweb.com>
2015-06-03 12:17:35 -0500

    Copy appSanitize function from /app/init to be used in import_script.sh
    Signed-off-by: Joseph A. Lutz <joseph.lutz@novatechweb.com>

--- docker-gitlab/import_script.sh
+++ docker-gitlab/import_script.sh
@@ -3,6 +3,28 @@
 
 set -e
 
+# this function comes from /app/init
+appSanitize () {
+  echo "Checking repository directories permissions..."
+  chmod -R ug+rwX,o-rwx ${GITLAB_DATA_DIR}/repositories/
+  chmod -R ug-s ${GITLAB_DATA_DIR}/repositories/
+  find ${GITLAB_DATA_DIR}/repositories/ -type d -print0 | xargs -0 chmod g+s
+  chown -R git:git ${GITLAB_DATA_DIR}/repositories
+
+  echo "Checking satellites directories permissions..."
+  sudo -u git -H mkdir -p ${GITLAB_DATA_DIR}/gitlab-satellites/
+  chmod u+rwx,g=rx,o-rwx ${GITLAB_DATA_DIR}/gitlab-satellites
+  chown -R git:git ${GITLAB_DATA_DIR}/gitlab-satellites
+
+  echo "Checking uploads directory permissions..."
+  chmod -R u+rwX ${GITLAB_DATA_DIR}/uploads/
+  chown git:git -R ${GITLAB_DATA_DIR}/uploads/
+
+  echo "Checking tmp directory permissions..."
+  chmod -R u+rwX ${GITLAB_DATA_DIR}/tmp/
+  chown git:git -R ${GITLAB_DATA_DIR}/tmp/
+}
+
 for archive_file in $(ls -1 /tmp/import_export/*.git*tar.gz)
 do
     repository=$(basename ${archive_file} | sed 's|\.git\..*|.git|')
